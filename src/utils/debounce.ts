bebf11683f4d904f941d640adcd9444d529c84cc
its-lotfy <mohammadlotfy97@gmail.com>
2025-09-30 20:52:49 +0330

    add debounce function for performance

---
+++ src/utils/debounce.ts
@@ -0,0 +1,10 @@
+export function debounce<Args extends unknown[]>(
+  func: (...args: Args) => void,
+  wait: number
+) {
+  let timeout: number;
+  return (...args: Args) => {
+    clearTimeout(timeout);
+    timeout = window.setTimeout(() => func(...args), wait);
+  };
+}
